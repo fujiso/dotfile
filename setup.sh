0631f8b35d480854164dbdcdab1c960cb96bda38
fujiso <fujiso@lr.pi.titech.ac.jp>
2018-12-18 20:17:50 +0900

    install vim-plug

--- setup.sh
+++ setup.sh
@@ -6,17 +6,20 @@ if [ ! -e ~/.zprezto ]
 then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
+
 echo "link config file..."
 ln -si .vimrc ~/.vimrc
 ln -si colors ... ~/.vim/
 ln -si .zshrc ~/.zshrc
 ln -si .zpreztorc ~/.zpreztorc
 ln -si .tmux.conf ~/.tmux.conf
+
 if [ ! -e ~/.pyenv ]
 then
     echo "setup pyenv..."
     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 fi
+
 echo "setup anaconda..."
 latest=$(~/.pyenv/bin/pyenv install -l | grep anaconda3 | tail -n 1)
 ~/.pyenv/bin/pyenv install $latest
