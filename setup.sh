a7b20586951700f4d395dda94810102fa4884139
fujiso <fujiso@lr.pi.titech.ac.jp>
2018-12-18 20:22:01 +0900

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,18 @@ then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
 
+# vim-plug for vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# vim-plug for neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 echo "link config file..."
 ln -si .vimrc ~/.vimrc
 ln -si colors ... ~/.vim/
 ln -si .zshrc ~/.zshrc
 ln -si .zpreztorc ~/.zpreztorc
 ln -si .tmux.conf ~/.tmux.conf
+ln -si init.vim ~/.config/nvim/init.vim
 
 if [ ! -e ~/.pyenv ]
 then
