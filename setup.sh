85736833afa9567adab772cbc74ab29354e339aa
fujiso <fujiso@lr.pi.titech.ac.jp>
2019-04-25 16:27:04 +0000

    add path to linuxbrew

--- setup.sh
+++ setup.sh
@@ -58,7 +58,7 @@ dddbrew > /dev/null 2>&1
 if [ $? -eq 127 ]; then
     echo "install lnuxbrew"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-    echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' >> $CONF
+    echo 'export PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"' >> $CONF
     echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' >> $CONF
     echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' >> $CONF
     source $CONF
